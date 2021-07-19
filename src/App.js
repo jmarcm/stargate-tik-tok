fb5d1e3d715367d41562deffb408a842f5d9632f
Jean-Marc <mjean_marc@hotmail.com>
2021-07-19 18:22:02 +0200

    Add Header

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import { HashRouter, Switch, Route } from 'react-router-dom';
 import './App.css';
 
+import Header from './components/Header';
+
 import Home from './Pages/Home';
 import Upload from './Pages/Upload';
 
 function App() {
     return (
         <HashRouter>
+            <Header />
             <Switch>
                 <Route path='/upload' component={Upload} />
                 <Route path='/' component={Home} />
