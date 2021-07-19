e8e217d6556d903388200b370ec43e75d08aba7b
Jean-Marc <mjean_marc@hotmail.com>
2021-07-19 19:02:53 +0200

    Add components on home page

---
+++ src/components/FollowersColumn.js
@@ -0,0 +1,9 @@
+function FollowersColumn() {
+    return (
+        <div className="follower-column">
+            <h1>Followers Column</h1>
+        </div>
+    )
+}
+
+export default FollowersColumn;
